090c58cfb00814fb2d00d099e8dc128d97525f89
islamgohst <33606410+islamgohst@users.noreply.github.com>
2018-06-05 17:12:45 +0100

    Create ControllerOrder.java

---
+++ interfaces/ControllerOrder.java
@@ -0,0 +1,31 @@
+package contract;
+
+/**
+ * The Enum ControllerOrder.
+ * List all Map and hero movements
+ * @author GR 1-MehdiLayadi-SissaouiIslam-AmineDergal-BougandouraYoucef
+ */
+public enum ControllerOrder {
+	MOVEUP,
+	MOVEDOWN,
+	MOVERIGHT,
+	MOVELEFT,
+	MAP1,
+	MAP2,
+	MAP3,
+	MAP4,
+	MAP5,
+	MAP6,
+	MAP7,
+	MAP8,
+	MAP9,
+	TEST,
+	MENU,
+	WORKSHOP,
+    FIRE,
+    ESC,
+    CLICK_MAP2,
+    CLICK_MAP3,
+    CLICK_MAP4,
+    CLICK_MAP5
+}
