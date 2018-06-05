1ee2282f5ba4b17f74f1867eb120bb3900a8301d
islamgohst <33606410+islamgohst@users.noreply.github.com>
2018-06-05 17:15:23 +0100

    Create IFireball.java

---
+++ interfaces/IFireball.java
@@ -0,0 +1,26 @@
+package contract;
+
+/**
+ *Interface fireball
+ */
+public interface IFireball extends IMobile {
+    /**
+     * @param direction
+     * Initialize a direction
+     * @author GR 1-MehdiLayadi-SissaouiIslam-AmineDergal-BougandouraYoucef
+     */
+    void setDirection(MobileOrder direction);
+
+    /**
+     * The step is the speed of the fireball
+     * @return step : int for number of case the fireball pass
+     * @author Dorian
+     */
+    int getStep();
+
+    /**
+     * Method that changes the fireball sprite at each case of the array, so it's multicolour
+     
+     */
+    void animate();
+}
