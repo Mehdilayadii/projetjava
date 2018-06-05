e148817447cc27a95f4d31de85f533a3dc3f7c97
DergalAmine <33619391+DergalAmine@users.noreply.github.com>
2018-06-05 17:38:09 +0100

    Add files via upload

---
+++ model/mobile/Monster4.java
@@ -0,0 +1,31 @@
+package model.mobile;
+
+import contract.*;
+
+import java.awt.*;
+
+/**
+ * GR 1-MehdiLayadi-SissaouiIslam-AmineDergal-BougandouraYoucef
+ */
+
+/** Class used to create the monster 4 */
+public class Monster4 extends Mobile implements IMonster {
+
+    /** Method that instantiates the Monster4 object
+     * @param pos contains the monster's position
+     */
+    public Monster4(Point pos)
+    {
+        super("monster_4.png", true, pos);
+    }
+
+    @Override
+    /** Method used to get the direction of the monster
+     * @param heroPos contains the position of the hero so the monster knows what's up
+     * @param tileMap contains the map in the memory
+     * @return the monster's direction
+     */
+    public MobileOrder getDirection(Point heroPos, IElement[][] tileMap) {
+        return null;
+    }
+}
